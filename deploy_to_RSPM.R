997fe75fce9848b39415ef091706e37937348533
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2021-07-09 14:56:21 +0200

    v0.4.0

---
+++ deploy_to_RSPM.R
@@ -0,0 +1,5 @@
+# sudo -s
+# alias rspm='/opt/rstudio-pm/bin/rspm'
+# rspm create git-builder --url=git@git.scnbdx.fr:UVD/datalab/misc/bdxmetroidentity/bdxmetroidentity.git --source=internal-gitlab-src --build-trigger=tags --ssh-key=bm-gitlab-ssh-key
+#
+# rspm run git-builder --name=bdxmetroidentity --source=internal-gitlab-src
